89bd02f41f6cb160978cc87560c4913794359625
YS123SY <yosragueffrech2017@gmail.com>
2018-05-23 18:58:57 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import NavBar from "./component/NavBar";
 import Collection from "./component/Collection";
+import Footer from "./component/Footer";
 
 class App extends Component {
   render() {
     return (
-
       <Router>
         <div className="App">
           <NavBar />
           <Collection />
+          <Footer />
         </div>
       </Router>
     );
