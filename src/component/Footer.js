377e5ac02f1e55ff22bc4bd2f5a6f643af15e2df
YS123SY <yosragueffrech2017@gmail.com>
2018-05-24 10:29:05 +0200

    home page

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,30 +6,28 @@ class Footer extends React.Component {
     return (
       <div className="footer-main">
         <div className="info">
-          <div className="contact"> 
+          <div className="contact">
             <span className="text">Contact us</span>
             <span className="sub-text">+216 00 000 000</span>
             <span className="sub-text">test@test.com</span>
             <span className="sub-text">tunis,1008 rue ...</span>
           </div>
-         
+
           <div className="social-container">
             <span className="text">meet us on social media</span>
-            <div className="social">   
-              <a href="#" class="fa fa-facebook"></a>
-              <a href="#" class="fa fa-twitter"></a>
-              <a href="#" class="fa fa-google"></a>
-              <a href="#" class="fa fa-linkedin"></a>
-              <a href="#" class="fa fa-youtube"></a>
-              <a href="#" class="fa fa-instagram"></a></div>
-          
+            <div className="social">
+              <a href="#" class="fa fa-facebook" />
+              <a href="#" class="fa fa-twitter" />
+              <a href="#" class="fa fa-google" />
+              <a href="#" class="fa fa-linkedin" />
+              <a href="#" class="fa fa-youtube" />
+              <a href="#" class="fa fa-instagram" />
             </div>
-      </div>
-      <div className="copy">
-      <span className="rights">
-          © 2018 Copyright
-        </span></div>
-      
+          </div>
+        </div>
+        <div className="copy">
+          <span className="rights">© 2018 Copyright</span>
+        </div>
       </div>
     );
   }
--- src/component/Test.js
+++ src/component/Test.js
@@ -1,50 +1,49 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import '../styles/test.css'
+import "../styles/test.css";
 
 class Test extends Component {
-
   render() {
     return (
-        <div className="accordion">
+      <div className="accordion">
         <ul>
           <li tabindex="1">
             <div>
               <a href="#">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <h2>TAE BO</h2>
+                <p>Looking for your Tae Bo partner or team</p>
               </a>
             </div>
           </li>
           <li tabindex="2">
             <div>
               <a href="#">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <h2>BACHATA</h2>
+                <p>Looking for your Bachata partner or team</p>
               </a>
             </div>
           </li>
           <li tabindex="3">
             <div>
               <a href="#">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <h2>ZUMBA</h2>
+                <p>Looking for your Zumba partner or team</p>
               </a>
             </div>
           </li>
           <li tabindex="4">
             <div>
               <a href="#">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <h2>TANGO SALSA</h2>
+                <p>Looking for your Tango/Salsa partner or team</p>
               </a>
             </div>
           </li>
           <li tabindex="5">
             <div>
               <a href="#">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <h2>DANSE ORIENTALE</h2>
+                <p>Looking for your Oriental Dance partner or team</p>
               </a>
             </div>
           </li>
